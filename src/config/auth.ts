4b8aa16627a841fc2841836175e0aecb9b1f1e83
Caio Capua <caiocapua@hotmail.com>
2020-11-05 15:03:37 -0300

    token JWT e middlewares de autenticacao

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'd78973f3dd84e9f80f2ae664b770e642',
+    expiresIn: '1d',
+  },
+};
--- src/mappers/index.ts
+++ src/mappers/index.ts
@@ -1,7 +1,7 @@
 import User from '../models/Users';
 
 export default class UserMap {
-  public static toDTO(user: User) {
+  public static toDTO(user: User): any {
     return {
       id: user.id,
       name: user.name,
