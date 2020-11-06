1b9a64530a4d332c323e511e558d9ede8123586a
Caio Capua <caiocapua@hotmail.com>
2020-11-05 22:33:00 -0300

    Migration para adicionar coluna avatar na tabela users, arquivo de config para upload da imagem, service para regra de negocio para salvar/deletar uma imagem

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 import UserMap from '../mappers/index';
+import EnsureAuthenticated from '../middlewares/ensureAuthenticated';
+import UploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(UploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -20,5 +26,27 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  EnsureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      const mappedUser = UserMap.toDTO(user);
+
+      return response.json(mappedUser);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default usersRouter;
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes/index';
+import UploadConfig from './config/upload';
 
 import './database/index';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(UploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
